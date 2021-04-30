b73b0b63ddc14764956ef6a12af14050f117744f
marzgra <marzgra@o2.pl>
2021-04-30 21:08:38 +0200

    moje komentarze

--- Washing Machine App/src/com/capgemini/idolata/Program.java
+++ Washing Machine App/src/com/capgemini/idolata/Program.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.capgemini.idolata;
 
 /**
@@ -8,16 +5,20 @@ package com.capgemini.idolata;
  *
  */
 public class Program {
-	
-	private String programName = "";
-	private int programNumber;
-	private int spinSpeed;
-	private int defoultTime;
-	
-	public Program( String programName, int programNumber, int spinSpeed) {
-		this.programName=programName;
-		this.programNumber=programNumber;
-		this.spinSpeed=spinSpeed;
-	}
-	
+
+    //TODO wartość "" jest zbędna
+    private String programName = "";
+    private int programNumber;
+    private int spinSpeed;
+    private int defoultTime;
+
+    // TODO konstruktor powienien zawierać wszystkie pola - w tym defaultTime
+    // (opcjonalnie) TODO możesz pokuśić się o stworzenie Buildera wewnątrz tej klasy
+    public Program(String programName, int programNumber, int spinSpeed) {
+        this.programName = programName;
+        this.programNumber = programNumber;
+        this.spinSpeed = spinSpeed;
+    }
+
+    //TODO dodaj gettery - bez setterów!
 }
